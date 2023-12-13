95771a452bc65ce169fe15469ea3304722d00f50
Jack <jackdek11@gmail.com>
2023-12-13 20:22:11 +0200

    updated deploy setup script

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -10,12 +10,38 @@ printf "${BLUE}You have initiated a deploy${NC}\n"
 echo "---------------------------"
 echo ""
 printf "${BLUE}Do you want to continue? $(tput bold)(y/n): ${NC}"
-read response
+read continue
 
-if [[ "$response" =~ ^[Yy]$ ]]; then
-    echo "You chose to continue."
+if [[ "$continue" =~ ^[Yy]$ ]]; then
+    echo ""
 else
     echo "Til next time"
+    echo ""
     exit 0
+fi
+
+inventory="inventory.ini"
+
+# Check if the file exists
+if [ -e "$inventory" ]; then
+    printf "${BLUE}File '$inventory' found. Do you wish to deploy to $(tput bold)(y/n)?\n${NC}"
+    echo ""
+    cat "$inventory"
+    echo ""
+    read confirm
+    if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    echo ""
+    else
+        printf "${BLUE}Please delete the file or change it's content before retrying.\n"
+        echo ""
+        exit 0
+    fi
+else
+    echo "No '$inventory' file found."
+    printf "${BLUE}Please enter the remote host name (or IP) ro deploy to: ${NC}"
+    read host
+    touch "$inventory"
+    echo "[public_ip]" > "$inventory"
+    echo "$host" >> "$inventory"
 fi
 
