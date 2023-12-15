14d5a2b95386d1328dd28afaa74ead022fff86f6
Jack <jackdek11@gmail.com>
2023-12-15 13:17:18 +0200

    linted app with prettier

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -22,134 +22,134 @@ import {
   jobit,
   tripguide,
   threejs,
-} from "../assets";
+} from '../assets';
 
 export const navLinks = [
   {
-    id: "about",
-    title: "About",
+    id: 'about',
+    title: 'About',
   },
   {
-    id: "work",
-    title: "Work",
+    id: 'work',
+    title: 'Work',
   },
   {
-    id: "contact",
-    title: "Contact",
+    id: 'contact',
+    title: 'Contact',
   },
 ];
 
 const services = [
   {
-    title: "Fullstack Developer",
+    title: 'Fullstack Developer',
     icon: web,
   },
   {
-    title: "DevOps Engineer",
+    title: 'DevOps Engineer',
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: 'Backend Developer',
     icon: backend,
   },
   {
-    title: "Cloud Engineer",
+    title: 'Cloud Engineer',
     icon: creator,
   },
 ];
 
 const technologies = [
   {
-    name: "HTML 5",
+    name: 'HTML 5',
     icon: html,
   },
   {
-    name: "CSS 3",
+    name: 'CSS 3',
     icon: css,
   },
   {
-    name: "JavaScript",
+    name: 'JavaScript',
     icon: javascript,
   },
   {
-    name: "TypeScript",
+    name: 'TypeScript',
     icon: typescript,
   },
   {
-    name: "React JS",
+    name: 'React JS',
     icon: reactjs,
   },
   {
-    name: "Redux Toolkit",
+    name: 'Redux Toolkit',
     icon: redux,
   },
   {
-    name: "Tailwind CSS",
+    name: 'Tailwind CSS',
     icon: tailwind,
   },
   {
-    name: "Node JS",
+    name: 'Node JS',
     icon: nodejs,
   },
   {
-    name: "MongoDB",
+    name: 'MongoDB',
     icon: mongodb,
   },
   {
-    name: "Three JS",
+    name: 'Three JS',
     icon: threejs,
   },
   {
-    name: "git",
+    name: 'git',
     icon: git,
   },
   {
-    name: "figma",
+    name: 'figma',
     icon: figma,
   },
   {
-    name: "docker",
+    name: 'docker',
     icon: docker,
   },
 ];
 
 const experiences = [
   {
-    title: "Backend developer",
-    company_name: "Cyber-Mint (Pty) Ltd",
+    title: 'Backend developer',
+    company_name: 'Cyber-Mint (Pty) Ltd',
     icon: cybermint,
-    iconBg: "#383E56",
-    date: "March 2021 - September 2023",
+    iconBg: '#383E56',
+    date: 'March 2021 - September 2023',
     points: [
       "Solutions architected the Django Backend for the company's main project, Teamfu.",
-      "Collaborated with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Set up CI / CD, containerization, and orchestration for various projects.",
-      "Participated in code reviews and provided constructive feedback to other developers.",
-      "Contributed to Frontend development and design in VueJS."
+      'Collaborated with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Set up CI / CD, containerization, and orchestration for various projects.',
+      'Participated in code reviews and provided constructive feedback to other developers.',
+      'Contributed to Frontend development and design in VueJS.',
     ],
   },
   {
-    title: "Backend developer",
-    company_name: "Currency Assist (Pty) Ltd",
+    title: 'Backend developer',
+    company_name: 'Currency Assist (Pty) Ltd',
     icon: currencyassist,
-    iconBg: "#383E56",
-    date: "Jan 2021 - Feb 2022",
+    iconBg: '#383E56',
+    date: 'Jan 2021 - Feb 2022',
     points: [
       "Built a django backend to aid in automating many of the bussiness's manual tasks.",
-      "Collaborated with clients and team members to define and complete requiremented tasks.",
-      "Addressed technical debt and broken legacy code.",
+      'Collaborated with clients and team members to define and complete requiremented tasks.',
+      'Addressed technical debt and broken legacy code.',
     ],
   },
   {
-    title: "Junior DevOps Engineer",
-    company_name: "Digitata (Pty) Ltd",
+    title: 'Junior DevOps Engineer',
+    company_name: 'Digitata (Pty) Ltd',
     icon: digitata,
-    iconBg: "#383E56",
-    date: "Oct 2023 - Present",
+    iconBg: '#383E56',
+    date: 'Oct 2023 - Present',
     points: [
-      "Developing and maintaining web applications using Vue.js and other related technologies.",
-      "Collaborating cross-departmentally to optimize customer experience, incident resolution, and strategic planning",
-      "Maintaining and putting existing deploy architecture into code, using ansible, terrafrom and related tools",
+      'Developing and maintaining web applications using Vue.js and other related technologies.',
+      'Collaborating cross-departmentally to optimize customer experience, incident resolution, and strategic planning',
+      'Maintaining and putting existing deploy architecture into code, using ansible, terrafrom and related tools',
     ],
   },
 ];
@@ -157,93 +157,93 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
   },
   {
     testimonial:
       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
   },
 ];
 
 const projects = [
   {
-    name: "Car Rent",
+    name: 'Car Rent',
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
+        name: 'react',
+        color: 'blue-text-gradient',
       },
       {
-        name: "mongodb",
-        color: "green-text-gradient",
+        name: 'mongodb',
+        color: 'green-text-gradient',
       },
       {
-        name: "tailwind",
-        color: "pink-text-gradient",
+        name: 'tailwind',
+        color: 'pink-text-gradient',
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: 'https://github.com/',
   },
   {
-    name: "Job IT",
+    name: 'Job IT',
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      'Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.',
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
+        name: 'react',
+        color: 'blue-text-gradient',
       },
       {
-        name: "restapi",
-        color: "green-text-gradient",
+        name: 'restapi',
+        color: 'green-text-gradient',
       },
       {
-        name: "scss",
-        color: "pink-text-gradient",
+        name: 'scss',
+        color: 'pink-text-gradient',
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
+    source_code_link: 'https://github.com/',
   },
   {
-    name: "Trip Guide",
+    name: 'Trip Guide',
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
     tags: [
       {
-        name: "nextjs",
-        color: "blue-text-gradient",
+        name: 'nextjs',
+        color: 'blue-text-gradient',
       },
       {
-        name: "supabase",
-        color: "green-text-gradient",
+        name: 'supabase',
+        color: 'green-text-gradient',
       },
       {
-        name: "css",
-        color: "pink-text-gradient",
+        name: 'css',
+        color: 'pink-text-gradient',
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: 'https://github.com/',
   },
 ];
 
