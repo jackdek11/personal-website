31cb416324bcbf7b54f569cfd4aeb87269a88d2c
Jack <jackdek11@gmail.com>
2024-01-11 23:15:34 +0200

    Added to onhover to ServiceCard

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Tilt } from 'react-tilt';
+import { React, useState } from 'react';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
@@ -7,34 +6,75 @@ import { services } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
 
-const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className="xs:w-[250px] w-full">
-    <motion.div
-      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+const ServiceCard = ({ index, title, icon, hoverText }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const variantsBox = {
+    animateHover: {
+      transition: {
+        duration: 0.4,
+        ease: 'easeOut',
+      },
+      opacity: 1,
+      y: 0,
+      scale: 1.1, // Add scale for enlargement
+    },
+    // ...
+  };
+
+  const variants = {
+    animateHover: {
+      transition: {
+        duration: 0.4, // Adjust duration as needed
+        ease: 'easeOut',
+      },
+      opacity: 1,
+      y: 0,
+    },
+    animateOut: {
+      transition: {
+        duration: 0.3, // Adjust duration as needed
+        ease: 'easeIn',
+      },
+      opacity: 0.5,
+      y: 3, // Adjust the starting position for the outgoing text
+    },
+  };
+
+  return (
+    <div className="xs:w-[250px] w-full">
+      <motion.div
+        variants={variantsBox}
+        animate={isHovered ? 'animateHover' : 'animateOut'}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-        <img
-          src={icon}
-          alt="web-development"
-          className="w-16 h-16 object-contain"
-        />
-
-        <h3 className="text-white text-[20px] font-bold text-center">
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
-
+        <div
+          options={{
+            max: 4,
+            scale: 1,
+            speed: 45,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <img
+            src={icon}
+            alt="web-development"
+            className="w-16 h-16 object-contain"
+          />
+          <motion.h3
+            className="text-white text-[20px] font-bold text-center"
+            variants={variants}
+            animate={isHovered ? 'animateHover' : 'animateOut'}
+          >
+            {isHovered ? hoverText : title}
+          </motion.h3>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
 const About = () => {
   return (
     <>
