e946b4944e4da0ee84504e6a03274d5468486a6e
Jack <jackdek11@gmail.com>
2024-09-24 22:53:50 +0200

    Updated service card section design

--- frontend/src/components/ServiceCard/ServiceCard.jsx
+++ frontend/src/components/ServiceCard/ServiceCard.jsx
@@ -14,7 +14,7 @@ const ServiceCard = ({ index, title, icon, hoverText }) => {
       <motion.div
         variants={ServiceCardVariantsBox}
         animate={isHovered ? 'animateHover' : 'animateOut'}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        className="w-full p-[1px] rounded-[20px] bg-[#33333]"
       >
         <div
           options={{
@@ -22,15 +22,10 @@ const ServiceCard = ({ index, title, icon, hoverText }) => {
             scale: 15,
             speed: 45,
           }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          className="bg-tertiary/40 rounded-[20px] py-5 px-12 min-h-[120px] flex justify-evenly items-center flex-col"
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
-          <img
-            src={icon}
-            alt="web-development"
-            className="w-16 h-16 object-contain"
-          />
           <motion.h3
             className="text-white text-[20px] font-bold text-center"
             variants={ServiceCardVariants}
